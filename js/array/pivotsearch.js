ddfa6c2d2c27b95e94f9ae7280bb8f762f1532d7
Ooms Aurélien <aurelien.ooms@gmail.com>
2014-02-09 11:11:27 +0100

    oops

--- js/array/pivotsearch.js
+++ js/array/pivotsearch.js
@@ -6,8 +6,8 @@ var pivotsearch_t = function(comp, equa, pivot){
 		if (i === j) return {f:0, w:i};
 		var p = pivot(k, a, i, j)
 		if      (equa(k, a[p])) return {f:1, w:p};
-		else if (comp(k, a[p])) return binarysearch(k, a, i, p);
-		else                    return binarysearch(k, a, p + 1, j);
+		else if (comp(k, a[p])) return pivotsearch(k, a, i, p);
+		else                    return pivotsearch(k, a, p + 1, j);
 	};
 
 	return pivotsearch;
