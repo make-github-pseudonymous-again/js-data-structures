98817e3fa6da99853ad8d3a735dfb0b6ffa0a6e5
Ooms Aurélien <aurelien.ooms@gmail.com>
2014-02-10 11:40:12 +0100

    fix

--- js/array/mergesort.js
+++ js/array/mergesort.js
@@ -1,13 +1,13 @@
 
 
-var mergesort_t = function(tapemerge){
+var mergesort_t = function(merge){
 
 	var mergesort = function(a, i, j, c, m, n){
 		if(j - i >= 2){
 			var cut = Math.floor((i + j) / 2);
 			mergesort(a, i, cut, c, m, m + cut - i);
 			mergesort(a, cut, j, c, m + cut - i, n);
-			tapemerge(a, i, cut, a, cut, j, c, m, n);
+			merge(a, i, cut, a, cut, j, c, m, n);
 			for(var t = 0; t < j - i; ++t) a[i + t] = c[m + t];
 		}
 	};
