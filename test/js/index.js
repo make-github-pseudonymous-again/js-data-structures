b842c08db26c21a0ee11c01e3ff0bed2494ae468
Ooms Aurélien <aurelien.ooms@gmail.com>
2014-06-04 11:27:34 +0200

    upgrade recquire

--- test/js/index.js
+++ test/js/index.js
@@ -1,5 +1,5 @@
 var recquire_t = require('recquire');
 
-var recquire = recquire_t('algo-test', 'index.js', false, false);
+var recquire = recquire_t('algo-test', 'index.js', 'intro.js', 'outro.js', false, true);
 
 recquire(__dirname + '/src/', module.exports, 0);
