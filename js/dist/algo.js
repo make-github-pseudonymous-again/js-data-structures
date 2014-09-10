799fad2e937830b7cb6fb8d2937d3d5f91a1a4c3
aureooms <aurelien.ooms@gmail.com>
2014-09-10 23:51:20 +0200

    3sum algo n2

--- js/dist/algo.js
+++ js/dist/algo.js
@@ -3,6 +3,42 @@
 	'use strict';
 
 
+/* js/src/3sum */
+/* js/src/3sum/_3sum_n2.js */
+
+
+/**
+ * Hypothesis :
+ *   - A is sorted in increasing order
+ *   - B is sorted in increasing order
+ *   - |A| > 0
+ *   - |B| > 0
+ */
+
+var _3sum_n2 = function (A, ai, aj, B, bi, bj, C, ci, cj, fn) {
+
+	var hi, lo, a, b, c, v;
+
+	for (; ci < cj; ++ci) {
+		lo = ai;
+		hi = bj - 1;
+
+		do {
+			
+			a = A[lo];
+			b = B[hi];
+			c = C[ci];
+			v = a + b;
+
+			if (-c === v) fn(a, b, c);
+
+			if (-c < v) --hi;
+			else ++lo;
+
+		} while (lo < aj && hi >= bi);
+	}
+
+};
 /* js/src/adt */
 /* js/src/adt/binomial_queue.js */
 var binomial_queue_t = function(pred){
