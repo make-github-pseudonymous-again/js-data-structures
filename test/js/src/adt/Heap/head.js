0feb270ccebaebfe5391a0a3a8b0f90753f4f748
aureooms <aurelien.ooms@gmail.com>
2014-10-22 22:19:28 +0200

    update head test for d-ary heaps

--- test/js/src/adt/Heap/head.js
+++ test/js/src/adt/Heap/head.js
@@ -5,22 +5,21 @@ sort = require( "aureooms-js-sort" );
 itertools = require( "aureooms-js-itertools" );
 functools = require( "aureooms-js-functools" );
 
-all = function ( heapname, __BinomialHeap__, treename, BinomialTree, diffname, diff, n ) {
+all = function ( heapname, Heap, diffname, diff, n ) {
 
-	var title = util.format( "Binomial Heap head (%s, %s, %s, %d)", heapname, treename, diffname, n );
+	var title = util.format( "Heap head (%s, %s, %d)", heapname, diffname, n );
 
 	console.log( title );
 
 	test( title, function () {
 
-		var BinomialHeap, q, a, i, x, b;
+		var q, a, i, x, b;
 
-		BinomialHeap = __BinomialHeap__( BinomialTree );
-
-		q = new BinomialHeap( diff );
+		q = new Heap( diff );
 		a = [];
 
 		i = n;
+
 		while ( i-- ) {
 			x = Math.random();
 			q.push(x);
@@ -50,21 +49,95 @@ all = function ( heapname, __BinomialHeap__, treename, BinomialTree, diffname, d
 
 itertools.product( [
 
-[
-	["__BinomialHeap__", algo.__BinomialHeap__],
-	// ["__LazyBinomialHeap__", algo.__LazyBinomialHeap__]
-],
+	itertools.chain( [
 
-[
-	["BinomialTree", algo.BinomialTree],
-	["BinomialTreeWithParent", algo.BinomialTreeWithParent]
-],
+		itertools.map( functools.partial( functools.star,
 
-[
-	["increasing", sort.increasing],
-	["decreasing", sort.decreasing]
-],
+			function ( heapname, template, treename, treeconstructor ) {
 
-[1, 16, 17, 31, 32, 33, 127, 128, 129]
+				return [
+					heapname + ", " + treename,
+					functools.partial( functools.create,
+						[template( treeconstructor )]
+					)
+				];
+
+			} ),
+
+			itertools.product( [
+
+			[
+				["__BinomialHeap__", algo.__BinomialHeap__],
+				// ["__LazyBinomialHeap__", algo.__LazyBinomialHeap__]
+			],
+
+			[
+				["BinomialTree", algo.BinomialTree],
+				["BinomialTreeWithParent", algo.BinomialTreeWithParent]
+			]
+
+			], 1, [] ),
+
+			[]
+
+		),
+
+
+		itertools.map( functools.partial( functools.star,
+
+			function ( heapname, template, arityname, arity ) {
+
+				return [
+					heapname + ", " + arityname,
+					template( arity )
+				];
+
+			} ),
+
+			itertools.product( [
+
+			[
+				[
+					"DAryHeapWithoutReferences",
+					functools.curry(
+						functools.partial(
+							functools.create, [algo.DAryHeapWithoutReferences]
+						),
+						2
+					)
+				],
+				[
+					"DAryHeap",
+					functools.curry(
+						functools.partial(
+							functools.create, [algo.DAryHeap]
+						),
+						2
+					)
+				]
+			],
+
+			[
+				["unary", 1],
+				["binary", 2],
+				["ternary", 3],
+				["4-ary", 4],
+				["5-ary", 5]
+			],
+
+			], 1, [] ),
+
+			[]
+
+		),
+
+	], [] ),
+
+	[
+		["increasing", sort.increasing],
+		["decreasing", sort.decreasing]
+	],
+
+	[1, 16, 17, 31, 32, 33, 127, 128, 129]
 
 ], 1, [] ).forEach( functools.partial( functools.star, [all] ) );
