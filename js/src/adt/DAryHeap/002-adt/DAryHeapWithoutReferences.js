688e0f726d047b1f0c0877a741211567dd3c256e
aureooms <aurelien.ooms@gmail.com>
2014-10-22 08:36:16 +0200

    Simple d-ary Heap implementation backed by an array

---
+++ js/src/adt/DAryHeap/002-adt/DAryHeapWithoutReferences.js
@@ -0,0 +1,89 @@
+
+
+var DAryHeapWithoutReferences = function ( arity, compare ) {
+
+	// arity of this heap
+
+	this.arity = arity;
+
+
+	// the comparison function
+
+	this.compare = compare;
+
+
+	// array used to store values
+
+	this.array = [];
+
+
+	// size of the heap
+
+	this.length = 0;
+
+};
+
+DAryHeapWithoutReferences.prototype.swap = function ( a, i, j ) {
+
+	var tmp;
+
+	tmp = a[i];
+	a[i] = a[j];
+	a[j] = tmp;
+
+};
+
+
+DAryHeapWithoutReferences.prototype.pop = function () {
+
+	var a, value;
+
+	if ( this.length === 0 ) {
+		return undefined;
+	}
+
+	--this.length;
+
+	a = this.array;
+
+	value = daryheap.pop( this.arity, this.compare, this.swap, a, 0, a.length );
+
+	a.pop();
+
+	return value;
+
+};
+
+
+DAryHeapWithoutReferences.prototype.push = function ( value ) {
+
+	var a;
+
+	++this.length;
+
+	a = this.array;
+
+	a.push( value );
+
+	daryheap.push( this.arity, this.compare, this.swap, a, 0, a.length - 1 );
+
+};
+
+DAryHeapWithoutReferences.prototype.merge = function ( other ) {
+
+	var a, i, j, k;
+
+	a = this.array;
+
+	i = 0;
+	j = a.length;
+
+	a = a.concat( other.array );
+
+	k = a.length;
+
+	daryheap.merge( this.arity, this.compare, this.swap, a, i, j, k );
+
+};
+
+exports.DAryHeapWithoutReferences = DAryHeapWithoutReferences;
