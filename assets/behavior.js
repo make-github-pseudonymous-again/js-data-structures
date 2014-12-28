a4a7344b32fd883209308b6278ed8cf9f1a90a80
aureooms <aurelien.ooms@gmail.com>
2014-12-28 19:13:58 +0100

    Generated documentation for 4156f8ad012ef6fe8990eff777e10e10056b712c

--- assets/behavior.js
+++ assets/behavior.js
@@ -763,6 +763,92 @@ f.event={add:function(a,c,d,e,g){var h,i,j,k,l,m,n,o,p,q,r,s;if(!(a.nodeType===3
           "outline": []
         }
       ]
+    }, {
+      "type": "folder",
+      "data": {
+        "path": "ksum",
+        "title": "ksum"
+      },
+      "depth": 1,
+      "children": [
+        {
+          "type": "file",
+          "data": {
+            "language": {
+              "nameMatchers": [".js"],
+              "pygmentsLexer": "javascript",
+              "multiLineComment": ["/*", "*", "*/"],
+              "singleLineComment": ["//"],
+              "ignorePrefix": "}",
+              "foldPrefix": "^",
+              "name": "JavaScript"
+            },
+            "sourcePath": "/home/aureooms/dev/js/js-algo/js/src/ksum/sortxy_n3.js",
+            "projectPath": "js/src/ksum/sortxy_n3.js",
+            "targetPath": "ksum/sortxy_n3",
+            "pageTitle": "ksum/sortxy_n3",
+            "title": "sortxy_n3"
+          },
+          "depth": 2,
+          "outline": [
+            {
+              "type": "heading",
+              "data": {
+                "level": 2,
+                "title": "|              X              |",
+                "slug": "-x-"
+              },
+              "depth": 2
+            }, {
+              "type": "heading",
+              "data": {
+                "level": 2,
+                "title": "|              Y              |",
+                "slug": "-y-"
+              },
+              "depth": 2
+            }
+          ]
+        }, {
+          "type": "file",
+          "data": {
+            "language": {
+              "nameMatchers": [".js"],
+              "pygmentsLexer": "javascript",
+              "multiLineComment": ["/*", "*", "*/"],
+              "singleLineComment": ["//"],
+              "ignorePrefix": "}",
+              "foldPrefix": "^",
+              "name": "JavaScript"
+            },
+            "sourcePath": "/home/aureooms/dev/js/js-algo/js/src/ksum/sortxy_n4.js",
+            "projectPath": "js/src/ksum/sortxy_n4.js",
+            "targetPath": "ksum/sortxy_n4",
+            "pageTitle": "ksum/sortxy_n4",
+            "title": "sortxy_n4"
+          },
+          "depth": 2,
+          "outline": [
+            {
+              "type": "heading",
+              "data": {
+                "level": 2,
+                "title": "|              X              |",
+                "slug": "-x-"
+              },
+              "depth": 2
+            }, {
+              "type": "heading",
+              "data": {
+                "level": 2,
+                "title": "|              Y              |",
+                "slug": "-y-"
+              },
+              "depth": 2
+            }
+          ]
+        }
+      ]
     }
   ];
 
