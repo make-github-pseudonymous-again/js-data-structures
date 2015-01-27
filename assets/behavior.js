673d168c10bd8bb32c69c23d855e196e27566cb0
aureooms <aurelien.ooms@gmail.com>
2015-01-27 19:08:52 +0100

    undefined

--- assets/behavior.js
+++ assets/behavior.js
@@ -405,26 +405,6 @@ f.event={add:function(a,c,d,e,g){var h,i,j,k,l,m,n,o,p,q,r,s;if(!(a.nodeType===3
               ]
             }
           ]
-        }, {
-          "type": "file",
-          "data": {
-            "language": {
-              "nameMatchers": [".js"],
-              "pygmentsLexer": "javascript",
-              "multiLineComment": ["/*", "*", "*/"],
-              "singleLineComment": ["//"],
-              "ignorePrefix": "}",
-              "foldPrefix": "^",
-              "name": "JavaScript"
-            },
-            "sourcePath": "/home/aureooms/dev/js/js-data-structures/js/src/adt/DoublyLinkedList.js",
-            "projectPath": "js/src/adt/DoublyLinkedList.js",
-            "targetPath": "adt/DoublyLinkedList",
-            "pageTitle": "adt/DoublyLinkedList",
-            "title": "DoublyLinkedList"
-          },
-          "depth": 2,
-          "outline": []
         }
       ]
     }
